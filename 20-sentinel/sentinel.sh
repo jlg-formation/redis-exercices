d08a7f6807586bd98ca8850cb32202169e82a993
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-12 13:42:13 +0200

    dev: sentinel ok

--- 20-sentinel/sentinel.sh
+++ 20-sentinel/sentinel.sh
@@ -6,12 +6,14 @@ start() {
     cat > redis-master.conf <<EOF
 port 35000
 daemonize yes
+logfile 35000.log
 EOF
     redis-server redis-master.conf
 
     cat > redis-replica-1.conf <<EOF
 port 35001
 daemonize yes
+logfile 35001.log
 replicaof 127.0.0.1 35000
 repl-diskless-load on-empty-db
 EOF
@@ -20,6 +22,7 @@ EOF
     cat > redis-replica-2.conf <<EOF
 port 35002
 daemonize yes
+logfile 35002.log
 replicaof 127.0.0.1 35000
 repl-diskless-load on-empty-db
 EOF
@@ -33,27 +36,42 @@ stop() {
 }
 
 clear() {
-    rm -f *.conf
+    rm -f *.conf *.log
 }
 
 sentstart() {
-    cat > redis-sentinel-1.conf <<EOF
-port 45000
+    for PORT in 45000 45001 45002
+    do
+       cat > redis-sentinel-${PORT}.conf <<EOF
+port ${PORT}
 daemonize yes
+logfile ${PORT}.log
 sentinel monitor mymaster 127.0.0.1 35000 2
 sentinel down-after-milliseconds mymaster 3000
 sentinel failover-timeout mymaster 6000
 sentinel parallel-syncs mymaster 1
 EOF
+        set -x
+        redis-server redis-sentinel-${PORT}.conf --sentinel 
+        set +x
+    done
+
+
+    
 }
 
 sentstop() {
-    redis-cli -p 35000 shutdown nosave || echo
+    for PORT in 45000 45001 45002
+    do
+        set -x
+        redis-cli -p ${PORT} shutdown || echo
+        set +x
+    done
 }
 
 help() {
     cat <<EOF
-$0 start|stop
+$0 start|stop|sentstart|sentstop|clear
 
 start: Start 1 master and 2 replicas
 stop: stop all redis instances
@@ -79,6 +97,16 @@ set +e
     
     if [ "${1:-}" == "clear" ]; then
         clear
+        exit 0
+    fi
+
+    if [ "${1:-}" == "sentstart" ]; then
+        sentstart
+        exit 0
+    fi
+
+    if [ "${1:-}" == "sentstop" ]; then
+        sentstop
         exit 0
     fi
 
